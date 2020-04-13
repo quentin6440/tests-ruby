5ad92ad54ce32c5b207cd404bb059834103c7ef7
quentin6440 <quentin.alaphilippe@gmail.com>
2020-04-13 23:48:07 +0200

    first commit

---
+++ lib/01_temperature.rb
@@ -0,0 +1,9 @@
+def ftoc (temp)
+  temp = (temp.to_f-32)/9*5
+return temp
+end
+
+def ctof (temp)
+  temp = temp.to_f*9/5+32
+return temp
+end
---
+++ lib/02_calculator.rb
@@ -0,0 +1,8 @@
+def add (val)
+return val[0]+val[1]
+end
+
+def ctof (temp)
+  temp = temp.to_f*9/5+32
+return temp
+end
